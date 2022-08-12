b5c8f78364b7d7fbe41d05904b07e53b468ebddb
Don <101531305+don-yoon@users.noreply.github.com>
2022-08-12 15:37:33 -0700

    default student absence of 0

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,7 +1,11 @@
 class Student < ApplicationRecord
   belongs_to :user
   belongs_to :cohort
-
+  
+  after_initialize :set_defaults
+  def set_defaults
+    self.absences ||= 0
+  end
   # Broadcast changes in realtime with Hotwire
   after_create_commit  -> { broadcast_prepend_later_to :students, partial: "students/index", locals: { student: self } }
   after_update_commit  -> { broadcast_replace_later_to self }
